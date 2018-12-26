64ea7bb8255ba1335c417f81f68547383db8f93c
Jinsoo Choi <hello.python89@gmail.com>
2018-12-27 00:28:27 +0900

    create project concentration

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Jinsoo Choi on 27/12/2018.
+//  Copyright © 2018 Jinsoo Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
