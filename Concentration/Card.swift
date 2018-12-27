0fa4e4572d3f13300528d50ddd8a54b1e6881347
Jinsoo Choi <hello.python89@gmail.com>
2018-12-28 00:30:29 +0900

    add gitignore

---
+++ Concentration/Card.swift
@@ -0,0 +1,9 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Jinsoo Choi on 27/12/2018.
+//  Copyright © 2018 Jinsoo Choi. All rights reserved.
+//
+
+import Foundation
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,9 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Jinsoo Choi on 27/12/2018.
+//  Copyright © 2018 Jinsoo Choi. All rights reserved.
+//
+
+import Foundation
